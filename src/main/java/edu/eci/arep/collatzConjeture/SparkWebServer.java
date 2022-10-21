1847b7b33c8f02eaf222dd49182baf461a583650
Brayan Macías <brayandresmt@outlook.com>
2022-10-21 12:32:12 -0500

    Initial Spark Server

--- src/main/java/edu/eci/arep/collatzConjeture/SparkWebServer.java
+++ src/main/java/edu/eci/arep/collatzConjeture/SparkWebServer.java
@@ -1,8 +1,18 @@
 package edu.eci.arep.collatzConjeture;
 
+import static spark.Spark.*;
+
 public class SparkWebServer {
 
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+    public static void main(String... args){
+        port(getPort());
+        get("/hello", (req, res) -> "Hello, Docker!");
+    }
+
+    private static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567;
     }
 }
